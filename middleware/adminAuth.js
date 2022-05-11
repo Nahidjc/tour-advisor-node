a7d9ad8885d24764f35cb8b53d2d89123a5d3735
Nahidjc <nahidjc80@gmail.com>
2022-05-11 17:45:28 +0600

    admin middleware

---
+++ middleware/adminAuth.js
@@ -0,0 +1,33 @@
+const User = require("../models/userModels");
+const jwt = require("jsonwebtoken")
+const authAdmin = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+    console.log(token);
+    if (!token) {
+      return res.status(400).json({ message: "Invalid Authentication" });
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
+      if (err) {
+        return res.status(400).json({ message: "Invalid Authentication" });
+      }
+      
+      req.user = user;
+      const admin = await User.findOne({
+        _id: user.id,
+      });
+
+      if (admin.role === "user" || admin.role === "hotel") {
+        return res.status(400).json({ msg: "Admin Recources Access Denied." });
+      }
+
+      // req.mentor = mentor
+      next();
+
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+module.exports = authAdmin;
