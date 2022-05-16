5218b3b74d8b9a2a9a3f0343cc6351c596d7905c
Nahidjc <nahidjc80@gmail.com>
2022-05-16 16:25:03 +0600

    feedback get and post api done

--- models/feedbackModel.js
+++ models/feedbackModel.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const feedbackSchema = mongoose.Schema({
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
+        type: Object,
+        require: true,
     },
     rating: {
         type: Number,
--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,53 +1,48 @@
-const user= require('../middleware/auth')
+const user = require("../middleware/auth");
 const router = require("express").Router();
 const User = require("../models/userModel");
 const Feedback = require("../models/feedbackModel");
-router.get("/feedback",async(req,res)=>{
-   
-    try {
-        const { rating,comment} = req.body;
-        if (  !rating || !comment) {
-            return res.status(400).json({ msg: "Invalid Feedback Credentials." });
-          }
-          const user = req.user.id;
-          const author = await User.findOne({ _id: user });
-          const newFeedback = new Feedback({
-              user,
-              rating,
-              fullName:author.fullName,
-              comment
-          })
-      res.json({
-        status: "Successfully feedback done",
-       
-      });
-    } catch (error) {
-      return res.status(500).json({ msg: error.message });
-    }
-  });
 
-  router.post("/feedback",user,async(req,res)=>{
+
+router.get("", async (req, res) => {
     try {
-        const { rating,comment} = req.body;
-        if (  !rating || !comment) {
-            return res.status(400).json({ msg: "Invalid Feedback Credentials." });
-          }
-          const user = req.user.id;
-          const author = await User.findOne({ _id: user });
-          const newFeedback = new Feedback({
-              user,
-              rating,
-              fullName:author.fullName,
-              comment
-          })
-          await newFeedback.save();
-      res.json({
-        status: "Successfully feedback done",
-       
-      });
+        console.log("Get feedback");
+        const feedbacks = await Feedback.find().select({
+            "createdAt": 0,
+            "updatedAt": 0,
+            "__v": 0
+        }).populate("user")
+        res.json({
+            result: feedbacks.length,
+            feedbacks,
+            status: "Successfully feedback done",
+        });
     } catch (error) {
-      return res.status(500).json({ msg: error.message });
+        return res.status(500).json({ msg: error.message });
     }
-  });
-  
+});
+
+router.post("", user, async (req, res) => {
+    try {
+        const { rating, comment } = req.body;
+        if (!rating || !comment) {
+            return res.status(400).json({ msg: "Invalid Feedback Credentials." });
+        }
+        const user = req.user.id;
+        const author = await User.findOne({ _id: user });
+        const newFeedback = new Feedback({
+            user:author,
+            rating,
+            fullName: author.fullName,
+            comment,
+        });
+        await newFeedback.save();
+        res.json({
+            status: "Successfully feedback done",
+        });
+    } catch (error) {
+        return res.status(500).json({ msg: error.message });
+    }
+});
+
 module.exports = router;
